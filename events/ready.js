f6afedd30d68f5841f2944778ef229018f3cd054
Noah G <noahg250602@gmail.com>
2024-03-02 03:49:46 +0100

    Add Autoclose feature with cron

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,42 @@
 const { Events } = require('discord.js');
+const autoClose = require('../utils/autoClose.js');
+
+const ENABLE_AUTO_CLOSE_TICKET = process.env.ENABLE_AUTO_CLOSE_TICKET;
+const AUTOCLOSE_CRON_CONFIG = process.env.AUTOCLOSE_CRON_CONFIG;
+const AUTOCLOSE_CRON_TIMEZONE = process.env.AUTOCLOSE_CRON_TIMEZONE;
+
+let cron = null;
+if (ENABLE_AUTO_CLOSE_TICKET) {
+	try {
+		cron = require('node-cron');
+	}
+	catch (e) {
+		console.log('unable to load cron module to schedule automatic ticket deletion');
+		console.log(e);
+	}
+
+}
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		// automatic closing of inactive tickets :
+		//  If the option is enabled, and cron is available on the system, run the task
+		if (ENABLE_AUTO_CLOSE_TICKET && cron != null) {
+			try {
+				cron.schedule(AUTOCLOSE_CRON_CONFIG, async () => {
+					await autoClose.autoCloseMain(client);
+				}, {
+					scheduled: true,
+					timezone: AUTOCLOSE_CRON_TIMEZONE,
+				});
+			} catch (e) {
+				/* empty */
+			}
+
+		}
 	},
 };
---
+++ utils/autoClose.js
@@ -0,0 +1,53 @@
+const utilities = require('./functions.js');
+
+const SUPPORT_CHANNEL_FORUM_ID = process.env.SUPPORT_CHANNEL_FORUM_ID;
+const TICKET_AUTO_CLOSED_MSG = process.env.TICKET_AUTO_CLOSED_MSG;
+const TIME_BEFORE_CLOSE_IN_HOURS = process.env.TIME_BEFORE_CLOSE_IN_HOURS;
+const TICKET_LAST_MSG_WHEN_CLOSED = process.env.TICKET_LAST_MSG_WHEN_CLOSED;
+const TICKET_CLOSE_ASK = process.env.TICKET_CLOSE_ASK;
+
+async function autoCloseMain(client) {
+    try {
+        // Get the support forum with his ID
+        const forumChannel = await client.channels.fetch(SUPPORT_CHANNEL_FORUM_ID);
+        if (!forumChannel) {
+            console.log('Channel not found');
+            return;
+        }
+
+        // Verify the type
+        if (forumChannel.type !== 15) {
+            console.log('The channel is not a forum');
+            return;
+        }
+
+        // Get All Active threads
+        const threads = await forumChannel.threads.fetchActive();
+        if (threads.threads.size === 0) {
+            // Not active thread
+            return;
+        }
+
+        // For each ticket
+        for (const thread of threads.threads.values()) {
+            // checks whether the thread's last message is older than X hours
+            const lastMessage = await thread.messages.fetch(thread.lastMessageId);
+            const XHoursAgo = Date.now() - TIME_BEFORE_CLOSE_IN_HOURS * 60 * 60 * 1000;
+
+            if (lastMessage.createdTimestamp < XHoursAgo) {
+                // Log last message if older than X hours
+                const isSupportMessage = await utilities.checkIfUserIDIsSupportMember(thread, lastMessage.author.id);
+                // If the last message is a message from support OR bot ask to close the ticket
+                if (lastMessage.content == TICKET_CLOSE_ASK || isSupportMessage) {
+                    // Close the ticket
+                    thread.send(TICKET_AUTO_CLOSED_MSG);
+                    thread.send(TICKET_LAST_MSG_WHEN_CLOSED);
+                }
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = { autoCloseMain };
